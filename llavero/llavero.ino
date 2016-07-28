7caf3d124dcac74df8b4abb4086dbf4af09085d4
Esteban Torre <esteban.torre@gmail.com>
2016-07-28 00:04:32 -0300

    Clean up

--- llavero/llavero.ino
+++ llavero/llavero.ino
@@ -47,8 +47,25 @@ enum LED_MODE {
 #define LED_BLINK_INTERVAL 500
 
 volatile LED_MODE led_status = LED_OFF;
+
 swRTC rtc;
+
 byte totp_secret[20];
+
+volatile int rand_seed = 1;
+
+volatile unsigned long last_push_high = 0;
+volatile byte last_push_value = LOW;
+
+volatile bool secret_set = false;
+uint8_t secret[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+char data[17];
+
+byte serial_buffer_ptr = 0;
+char serial_last_byte = 0;
+char serial_buffer[SERIAL_BUFFER_LEN + 1];
+
+char* error_answer = "ERROR\n";
 
 void process_led()
 {
@@ -70,7 +87,7 @@ void process_led()
   }
 }
 
-volatile int rand_seed = 1;
+
 void rand_init()
 {
   for (int i = 0; i < 10; ++i)
@@ -83,8 +100,6 @@ void rand_init()
   randomSeed(rand_seed);
 }
 
-volatile unsigned long last_push_high = 0;
-volatile byte last_push_value = LOW;
 void push_interrupt()
 {
   byte value = digitalRead(PUSH_PIN);
@@ -96,7 +111,7 @@ void push_interrupt()
     {
       if (last_push_high < now)
       {
-        command_confirmation();
+        cmd_confirmation();
       }
     }
     else
@@ -106,9 +121,6 @@ void push_interrupt()
   }
 }
 
-volatile bool secret_set = false;
-uint8_t secret[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-char data[17];
 
 byte n_for_hex(char x)
 {
@@ -139,25 +151,29 @@ bool ascii_hex_to_bin(char* ascii, byte* dest, byte expected_size, bool reverse 
     return false;
   }
   byte i = 2;
-  byte j = 0;
+  byte j;
+  
   if(reverse == true)
     j = (expected_size-1);
+  else
+    j = 0;
+
   while ((reverse == false && j < expected_size) || (reverse == true && j >= 0))
   {
     byte x = n_for_hex(ascii[i]);
     byte y = n_for_hex(ascii[i + 1]);
-    ascii[i] = 0; //Wipe key from buffer
-    ascii[i + 1] = 0;
-    i = i + 2;
     if (x > 15 || y > 15)
       return false;
+    
     dest[j] = (x << 4) | y;
+    
     if(reverse == true)
       j--;
     else
       j++;
-    
+    i = i + 2;
   }
+  memset(ascii,0,i);
   if (ascii[i] != 0) //Extra data, not good.
     return false;
   else
@@ -187,11 +203,7 @@ bool decrypt_data()
   return true;
 }
 
-char* error_answer = "ERROR\n";
 
-byte serial_buffer_ptr = 0;
-char serial_last_byte = 0;
-char serial_buffer[SERIAL_BUFFER_LEN + 1];
 
 void check_serial()
 {
@@ -229,10 +241,10 @@ void discard_serial()
 
 void process_serial()
 {
-  check_command();
+  check_cmd();
 }
 
-enum LLAVERO_COMMAND
+enum LLAVERO_cmd
 {
   GREETING, // Leave as first ALWAYS
   SET_SECRET,
@@ -242,38 +254,38 @@ enum LLAVERO_COMMAND
   INIT,
   TIME,
   SET_TOTP,
-  NO_COMMAND // Leave as last ALWAYS
+  NO_cmd // Leave as last ALWAYS
 };
-enum COMMAND_STATUS
+enum cmd_STATUS
 {
   READY,
   WAITING_CONFIRMATION,
   WAITING_ARGUMENTS
 };
 
-char *command_txt[] = {"hi", "secret", "set", "get", "ls", "init", "time", "sett"};
+char *cmd_txt[] = {"hi", "secret", "set", "get", "ls", "init", "time", "sett"};
 
-typedef void (* command_func) ();
-command_func command_recv_function[] = {greeting_command_recv, set_secret_command_recv,
-                                        set_password_command_recv, get_password_command_recv, list_tags_command_recv,
-                                        init_command_recv, time_command_recv, set_totp_command_recv
+typedef void (* cmd_func) ();
+cmd_func cmd_recv_fun[] = {greeting_cmd_recv, set_secret_cmd_recv,
+                                        set_password_cmd_recv, get_password_cmd_recv, list_tags_cmd_recv,
+                                        init_cmd_recv, time_cmd_recv, set_totp_cmd_recv
                                        };
-command_func command_arg_function[] = {command_noop, set_secret_command_arg,
-                                       set_password_command_arg, get_password_command_arg, command_noop,
-                                       command_noop, time_command_arg, set_totp_command_arg
+cmd_func cmd_arg_fun[] = {cmd_noop, set_secret_cmd_arg,
+                                       set_password_cmd_arg, get_password_cmd_arg, cmd_noop,
+                                       cmd_noop, time_cmd_arg, set_totp_cmd_arg
                                       };
-command_func command_confirm_function[] = {command_noop, command_noop,
-                                           command_noop, get_password_command_confirm, list_tags_command_confirm,
-                                           init_command_confirm, command_noop, command_noop
+cmd_func cmd_confirm_fun[] = {cmd_noop, cmd_noop,
+                                           cmd_noop, get_password_cmd_confirm, list_tags_cmd_confirm,
+                                           init_cmd_confirm, cmd_noop, cmd_noop
                                           };
 
-LLAVERO_COMMAND current_command = NO_COMMAND;
-COMMAND_STATUS command_status = READY;
+LLAVERO_cmd current_cmd = NO_cmd;
+cmd_STATUS cmd_status = READY;
 byte current_argument = -1;
-byte command_argument_len[MAX_ARGUMENTS];
-char command_argument[MAX_ARGUMENTS][SERIAL_BUFFER_LEN + 1];
+byte cmd_arg_len[MAX_ARGUMENTS];
+char cmd_arg[MAX_ARGUMENTS][SERIAL_BUFFER_LEN + 1];
 
-bool is_command(char* cmd, char* buff, byte buff_len)
+bool is_cmd(char* cmd, char* buff, byte buff_len)
 {
   if (buff_len <= 0)
     return false;
@@ -292,24 +304,24 @@ bool is_command(char* cmd, char* buff, byte buff_len)
   return (i == buff_len && cmd[i] == 0);
 }
 
-byte which_command(char* buff, byte buff_len)
+byte which_cmd(char* buff, byte buff_len)
 {
-  for (byte i = GREETING; i < NO_COMMAND; i++)
+  for (byte i = GREETING; i < NO_cmd; i++)
   {
-    if (is_command(command_txt[i], buff, buff_len))
+    if (is_cmd(cmd_txt[i], buff, buff_len))
       return i;
   }
-  return NO_COMMAND;
+  return NO_cmd;
 }
 
 bool read_argument()
 {
   if ((current_argument >= 0) && (current_argument <= MAX_ARGUMENTS))
   {
-    memcpy(command_argument[current_argument], serial_buffer, SERIAL_BUFFER_LEN);
+    memcpy(cmd_arg[current_argument], serial_buffer, SERIAL_BUFFER_LEN);
     memset(serial_buffer, 0, SERIAL_BUFFER_LEN);
-    command_argument[current_argument][SERIAL_BUFFER_LEN] = 0;
-    command_argument_len[current_argument] = serial_buffer_ptr;
+    cmd_arg[current_argument][SERIAL_BUFFER_LEN] = 0;
+    cmd_arg_len[current_argument] = serial_buffer_ptr;
     return true;
   } else
   {
@@ -320,141 +332,192 @@ bool read_argument()
 void wait_confirmation()
 {
   led_status = LED_BLINK;
-  command_status = WAITING_CONFIRMATION;
+  cmd_status = WAITING_CONFIRMATION;
 }
 
 void wait_argument(byte n)
 {
-  command_status = WAITING_ARGUMENTS;
+  cmd_status = WAITING_ARGUMENTS;
   current_argument = n;
 }
 
-void command_end()
+void cmd_end()
 {
   current_argument = -1;
-  current_command = NO_COMMAND;
-  command_status = READY;
+  current_cmd = NO_cmd;
+  cmd_status = READY;
   led_status = LED_OFF;
 }
 
-void command_noop()
+void cmd_noop()
 {
 
 }
 
-void greeting_command_recv()
+void greeting_cmd_recv()
 {
   Serial.print(F("HI HUMAN, I'M LLAVERO, A ROBOT FROM THE PAST MADE TO BURY YOUR SECRETS DEEP IN MY SILICON YARD."));
   print_time();
-  command_end();
+  cmd_end();
 }
 
-void set_secret_command_recv()
+void set_secret_cmd_recv()
 {
   Serial.print(F("ENTER THE SECRET.\n"));
   wait_argument(0);
 }
 
-void set_secret_command_arg()
+void set_secret_cmd_arg()
 {
   if (current_argument == 0)
   {
-    if (set_secret(command_argument[current_argument]))
+    if (set_secret(cmd_arg[current_argument]))
     {
-      command_ack();
-      command_end();
+      cmd_ack();
+      cmd_end();
     }
     else
     {
-      command_error();
+      cmd_error();
     }
   } else
   {
-    command_error();
+    cmd_error();
   }
 }
 
-void set_password_command_recv()
+void set_password_cmd_recv()
 {
   Serial.print(F("ENTER TAG (7 CHAR MAX).\n"));
-  eeprom_new();
   wait_argument(0);
 }
 
-void set_password_command_arg()
+bool save_password_for_tag(char* pass, byte pass_len, char* tag, byte tag_len)
+{
+  if(pass_len > 32 || tag_len > 7)
+    return false;
+    
+  eeprom_new();
+  EEPROM_ram_record* record = eeprom_current_record();
+  
+  record->flags = 0;
+  memcpy(record->tag, cmd_arg[0],7);
+  
+  memcpy(data, cmd_arg[1],16);
+  if(encrypt_data() == false)
+    return false;
+  memcpy(eeprom_current_record()->data1,data,16);
+  
+  if(cmd_arg_len[1] > 16)
+  {
+    record->flags = record->flags | EXTENDED;
+    memcpy(data, &cmd_arg[1][16],16);
+    if(encrypt_data() == false)
+      return false;
+    memcpy(eeprom_current_record()->data2,data,16);
+  } 
+  
+  eeprom_write();
+}
+
+void set_password_cmd_arg()
 {
   if (current_argument == 0)
   {
-    int l = command_argument_len[0];
+    int l = cmd_arg_len[0];
     if(l > 0 && l < 8)
     {
-      command_ack();
+      cmd_ack();
       Serial.print(F("SEND THE SECRET.\n"));
       wait_argument(1);
     }else
     {
-      command_error();
+      cmd_error();
     }
   } else if (current_argument == 1)
   {
-    if(command_argument_len[1] > 32)
-    {
-      command_error();
-    }
+    bool result = save_password_for_tag(cmd_arg[1], cmd_arg_len[1], cmd_arg[0], cmd_arg_len[0]);
+    if(result == false)
+      cmd_error();
     else
     {
-      command_ack();
-      eeprom_current_record()->flags = 0;
-      memcpy(data, command_argument[1],16);
-      encrypt_data();
-      memcpy(eeprom_current_record()->data1,data,16);
-      
-      if(command_argument_len[1] > 16)
-      {
-        eeprom_current_record()->flags = EXTENDED;
-        memcpy(data, &command_argument[1][16],16);
-        encrypt_data();
-        memcpy(eeprom_current_record()->data2,data,16);
-      } 
-      
-      memcpy(eeprom_current_record()->tag, command_argument[0],7);
-      eeprom_write();
-      command_end();
+      cmd_ack();
+      cmd_end();
     }
   } else
   {
-    command_error();
+    cmd_error();
   }
 }
 
-void get_password_command_recv()
+void get_password_cmd_recv()
 {
   Serial.print(F("ENTER TAG.\n"));
   wait_argument(0);
 }
 
-void get_password_command_arg()
+void get_password_cmd_arg()
 {
   if (current_argument == 0)
   {
-    command_ack();
-    int l = strlen(command_argument[current_argument]);
+    cmd_ack();
+    int l = strlen(cmd_arg[current_argument]);
     if(l>0 && l <8)
     {
       Serial.print(F("FOCUS ON PROMPT AND PUSH THE BUTTON.\n"));
       wait_confirmation();
     }
     else
-      command_error();
+      cmd_error();
   } else
   {
-    command_error();
+    cmd_error();
   }
 }
 
-void get_password_command_confirm()
+void print_totp(EEPROM_ram_record* record)
 {
-  bool result = eeprom_load_by_tag(command_argument[0]);
+  memcpy(data, record->data1,16);
+  decrypt_data();
+  memcpy(totp_secret, data, 16);
+  memcpy(data, record->data2,16);
+  decrypt_data();
+  memcpy(&totp_secret[16], data, 4);
+  TOTP totp = TOTP(totp_secret, 20,30);
+  long GMT = rtc.getTimestamp();
+  char* code = totp.getCode(GMT);
+  Keyboard.print(code);
+  Keyboard.print('\n');
+  memset(totp_secret,0,20);
+}
+
+bool access_record_by_tag(char* tag, byte tag_len)
+{
+  if(tag_len > 7)
+    return false;
+  bool found = eeprom_load_by_tag(tag);
+  if(found == false)
+    return false;
+    
+  EEPROM_ram_record* record = eeprom_current_record();
+  
+  //memcpy(data, record->data1,16);
+  //decrypt_data();
+  
+  if((eeprom_current_record()->flags&TOTP_RECORD) == TOTP_RECORD)
+  {
+    print_totp(record);
+  }else
+  {
+    Keyboard.print(data);
+    Keyboard.print('\n');
+    memset(data,0,16);
+  }
+}
+
+void get_password_cmd_confirm()
+{
+  bool result = eeprom_load_by_tag(cmd_arg[0]);
   if(result)
   {
     memcpy(data, eeprom_current_record()->data1,16);
@@ -481,34 +544,34 @@ void get_password_command_confirm()
   {
     Serial.print(F("NOT FOUND.\n"));
   }
-  command_end();
+  cmd_end();
 }
 
-void list_tags_command_recv()
+void list_tags_cmd_recv()
 {
   Serial.print(F("PUSH THE BUTTON.\n"));
   wait_confirmation();
 }
 
-void list_tags_command_confirm()
+void list_tags_cmd_confirm()
 {
   eeprom_print_all_tags();
-  command_end();
+  cmd_end();
 }
 
-void init_command_recv()
+void init_cmd_recv()
 {
   Serial.print(F("THIS WILL RESET THE DEVICE, PLEASE CONFIRM.\n"));
   wait_confirmation();
 }
 
-void init_command_confirm()
+void init_cmd_confirm()
 {
   eeprom_reset();
-  command_end();
+  cmd_end();
 }
 
-void time_command_recv()
+void time_cmd_recv()
 {
   Serial.print(F("I'D LOVE TO KNOW... WHAT TIME IS IT?\n"));
   wait_argument(0);
@@ -532,10 +595,10 @@ void print_time()
   Serial.print("\n");
 }
 
-void time_command_arg()
+void time_cmd_arg()
 {
-  command_ack();
-  ascii_hex_to_bin(command_argument[0],(byte*)&timestamp, 4, true);  
+  cmd_ack();
+  ascii_hex_to_bin(cmd_arg[0],(byte*)&timestamp, 4, true);  
   if(rtc.getYear() < 1970)
   {
     rtc.stopRTC();
@@ -543,36 +606,36 @@ void time_command_arg()
     rtc.startRTC();
   }  
   print_time();
-  command_end();
+  cmd_end();
 }
 
-void set_totp_command_recv()
+void set_totp_cmd_recv()
 {
   Serial.print(F("ENTER TAG.\n"));
   wait_argument(0);
 }
 
-void set_totp_command_arg()
+void set_totp_cmd_arg()
 {
   if (current_argument == 0)
   {
-    command_ack();
+    cmd_ack();
     Serial.print(F("NOW THE SECRET.\n"));
     wait_argument(1);
   }else
   {
-    command_ack();
+    cmd_ack();
     eeprom_current_record()->flags = TOTP_RECORD|EXTENDED;
-    ascii_hex_to_bin(command_argument[1],totp_secret, 20);
+    ascii_hex_to_bin(cmd_arg[1],totp_secret, 20);
     memcpy(data, totp_secret,16);
     encrypt_data();
     memcpy(eeprom_current_record()->data1,data,16);
     memcpy(data, &totp_secret[16],4);
     encrypt_data();
     memcpy(eeprom_current_record()->data2,data,16);
-    memcpy(eeprom_current_record()->tag, command_argument[0], 7);
+    memcpy(eeprom_current_record()->tag, cmd_arg[0], 7);
     eeprom_write();
-    command_end();
+    cmd_end();
   }
 }
 
@@ -581,43 +644,43 @@ void print_totp()
   
 }
 
-void command_ack()
+void cmd_ack()
 {
   Serial.write("ACK\n");
 }
 
-void command_error()
+void cmd_error()
 {
   Serial.write("ERROR\n");
-  command_end();
+  cmd_end();
 }
 
-void check_command()
+void check_cmd()
 {
-  switch (command_status)
+  switch (cmd_status)
   {
     case READY:
-      current_command = (LLAVERO_COMMAND) which_command(serial_buffer, serial_buffer_ptr);
-      if (current_command != NO_COMMAND)
+      current_cmd = (LLAVERO_cmd) which_cmd(serial_buffer, serial_buffer_ptr);
+      if (current_cmd != NO_cmd)
       {
-        command_ack();
-        command_recv_function[current_command]();
+        cmd_ack();
+        cmd_recv_fun[current_cmd]();
       } else
       {
-        command_error();
+        cmd_error();
       }
       break;
     case WAITING_CONFIRMATION:
       Serial.print(F("CONFIRMATION CANCELED.\n"));
-      command_error();
+      cmd_error();
       break;
     case WAITING_ARGUMENTS:
       if (read_argument())
       {
-        command_arg_function[current_command]();
+        cmd_arg_fun[current_cmd]();
       } else
       {
-        command_error();
+        cmd_error();
       }
       break;
   }
@@ -628,16 +691,16 @@ void default_action()
   Serial.print(F("TOUCH THIS!\n"));
 }
 
-void command_confirmation()
+void cmd_confirmation()
 {
-  if (current_command != NO_COMMAND &&
-      command_status == WAITING_CONFIRMATION)
+  if (current_cmd != NO_cmd &&
+      cmd_status == WAITING_CONFIRMATION)
   {
     delay(CONFIRMATION_TIME);
     if (digitalRead(PUSH_PIN) == HIGH)
     {
       led_status = LED_OFF;
-      command_confirm_function[current_command]();
+      cmd_confirm_fun[current_cmd]();
     } else
     {
       Serial.print(F("ERROR: PUSH ME CORRECTLY!.\n"));
